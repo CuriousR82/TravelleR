6141dbc80db82c184f6d638d43ad5ecfc904766e
CuriousR82 <rosajeon82@gmail.com>
2023-06-04 22:25:22 -0400

    Initial Commit

---
+++ TravelleR/TravelleRApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelleRApp.swift
+//  TravelleR
+//
+//  Created by Rosa Jeon on 2023-06-04.
+//
+
+import SwiftUI
+
+@main
+struct TravelleRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
